6c68eda583466d6ef0e87783bd98361e79f213f8
yves <yves@lepidus.com.br>
2025-09-23 09:48:22 -0400

    Adds plugin setup test

---
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -0,0 +1,10 @@
+describe('Setup Selection Of Reviewing Interests plugin', function () {
+    it('Enable the plugin in the plugins list', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.contains('a', 'Website').click();
+        cy.waitJQuery();
+        cy.get('#plugins-button').click();
+        cy.get('input[id^=select-cell-selectionofreviewinginterestsplugin]').check();
+        cy.get('input[id^=select-cell-selectionofreviewinginterestsplugin]').should('be.checked');
+    })
+});
