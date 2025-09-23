6e20237b35461a0a5d3d7876ebc5a2fd3c6a6da0
yves <yves@lepidus.com.br>
2025-09-23 11:51:22 -0400

    Implements plugin setup configuration test

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -7,4 +7,19 @@ describe('Setup Selection Of Reviewing Interests plugin', function () {
         cy.get('input[id^=select-cell-selectionofreviewinginterests]').check();
         cy.get('input[id^=select-cell-selectionofreviewinginterests]').should('be.checked');
     })
+    it('View reviewing interests options configuration', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.contains('a', 'Website').click();
+        cy.waitJQuery();
+        cy.get('#plugins-button').click();
+
+        const pluginRowId = 'component-grid-settings-plugins-settingsplugingrid-category-generic-row-selectionofreviewinginterests';
+
+        cy.get('tr#' + pluginRowId + ' a.show_extras').click();
+        cy.get('a[id^=' + pluginRowId + '-settings-button]').click();
+        cy.get('.pkp_modal_panel > :nth-child(1)').contains('Selection Field in Reviewing Interests Area');
+        cy.get('a[id^=component-plugins-generic-selectionofreviewinginterests-controllers-grid-selectionofreviewinginterestsgrid-addOption-button-]').contains('Add option').click();
+        cy.get('input[id^=optionName-]').should('be.visible');
+    });
+
 });
