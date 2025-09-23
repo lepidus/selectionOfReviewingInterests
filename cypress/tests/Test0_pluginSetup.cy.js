56149ed7a9971c323e8384e992db1db0db21b055
yves <yves@lepidus.com.br>
2025-09-23 15:34:57 -0400

    Fix plugin setup test

--- cypress/tests/Test0_pluginSetup.cy.js
+++ cypress/tests/Test0_pluginSetup.cy.js
@@ -1,4 +1,4 @@
-describe('Setup Selection Of Reviewing Interests plugin', function () {
+describe('Enable Selection Of Reviewing Interests plugin', function () {
     it('Enable the plugin in the plugins list', function () {
         cy.login('dbarnes', null, 'publicknowledge');
         cy.contains('a', 'Website').click();
@@ -18,8 +18,8 @@ describe('Setup Selection Of Reviewing Interests plugin', function () {
         cy.get('tr#' + pluginRowId + ' a.show_extras').click();
         cy.get('a[id^=' + pluginRowId + '-settings-button]').click();
         cy.get('.pkp_modal_panel > :nth-child(1)').contains('Selection Field in Reviewing Interests Area');
+        cy.waitJQuery();
         cy.get('a[id^=component-plugins-generic-selectionofreviewinginterests-controllers-grid-selectionofreviewinginterestsgrid-addOption-button-]').contains('Add option').click();
         cy.get('input[id^=optionName-]').should('be.visible');
     });
-
 });
