521896fade9ef4d337819c76cfdfd0a8194f75b9
yves <yves@lepidus.com.br>
2025-09-24 16:29:15 -0400

    Adds reviewers without interests validation

---
+++ cypress/tests/Test2_reviewersValidation.cy.js
@@ -0,0 +1,24 @@
+describe('Reviewers must not use OJS without reviewing interests', function () {
+    it('Reviewer without interests login', function () {
+        cy.login('agallego', null, 'publicknowledge');
+        cy.get('.pkpNotification').contains('You must select at least one reviewing interests in the "Roles" tab.').should('be.visible');
+
+        cy.get('.app__returnHeaderLink').click();
+        cy.get('.pkpNotification').contains('You must select at least one reviewing interests in the "Roles" tab.').should('be.visible');
+
+        cy.get('#ui-id-5').click();
+        cy.get('.interests').click();
+        cy.get('li[class=ui-menu-item').contains("Estudos teóricos e de campo em escalas que variam do local ao regional/global, abrangendo períodos de curta e longa duração, incluindo tempo geológico").click();
+
+        cy.get('#rolesForm > .formButtons > button[id^=submitFormButton]')
+                .click();
+        
+        cy.get('.app__returnHeaderLink').click();
+        cy.get('.app__pageHeading').contains('Submissions').should('be.visible');
+    });
+
+    it('Users without reviewer role must use OJS with any problems', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('.app__pageHeading').contains('Submissions').should('be.visible');
+    });
+});
