9cada831798ffacde1b0468796d39071e7606d03
iudi <iudi@lepidus.com.br>
2026-03-03 16:44:35 -0300

    fix: preserve user interests set on other contexts
    Signed-off-by: iudi <iudi@lepidus.com.br>
    Signed-off-by: carlos <carlos@lepidus.com.br>

--- js/interestsTagitPatch.js
+++ js/interestsTagitPatch.js
@@ -9,6 +9,11 @@
 				&& $.pkp.plugins.generic
 				&& $.pkp.plugins.generic.selectionOfReviewingInterests;
 			if (ns && ns.interestsOptions) {
+				var existingTags = originalTagit.call(this, 'assignedTags') || [];
+				var existingTagsLower = $.map(existingTags, function(tag) {
+					return tag.toLowerCase();
+				});
+
 				originalTagit.call(this, 'destroy');
 				var el = this;
 				originalTagit.call(this, {
@@ -18,12 +23,25 @@
 					autocomplete: {delay: 0, minLength: 0},
 					beforeTagAdded: function(event, ui) {
 						var availableTags = originalTagit.call(el, 'option', 'availableTags');
-						var tagAllowed = $.map(availableTags, function(tag) {
+						var tagLower = ui.tagLabel.toLowerCase();
+						var inAllowedList = $.map(availableTags, function(tag) {
 							return tag.toLowerCase();
-						}).indexOf(ui.tagLabel.toLowerCase()) !== -1;
-						return tagAllowed;
+						}).indexOf(tagLower) !== -1;
+						var isPreExisting = existingTagsLower.indexOf(tagLower) !== -1;
+						return inAllowedList || isPreExisting;
 					}
 				});
+
+				$.each(existingTags, function(i, tag) {
+					var currentTags = originalTagit.call(el, 'assignedTags') || [];
+					var currentTagsLower = $.map(currentTags, function(t) {
+						return t.toLowerCase();
+					});
+					if (currentTagsLower.indexOf(tag.toLowerCase()) === -1) {
+						originalTagit.call(el, 'createTag', tag);
+					}
+				});
+
 				$(this).data('soriReinit', true);
 				$(document)
 					.off('focus.sori click.sori', '.tagit-new input')
