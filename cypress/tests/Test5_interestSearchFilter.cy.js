132612c1c49a60187dbbc51f9d6e5043cee53dc6
carlos <carlos@lepidus.com.br>
2026-04-22 18:05:56 -0400

    Implement cypress test for interests filter
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1164
    
    Signed-off-by: carlos <carlos@lepidus.com.br>

---
+++ cypress/tests/Test5_interestSearchFilter.cy.js
@@ -0,0 +1,15 @@
+describe('Filtering reviews by interests', function () {
+    it('Accessing the interests filter', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('.listPanel__itemSubtitle:visible:contains("Finocchiaro: Arguments About Arguments")').first()
+        .parent().parent().within(() => {
+            cy.get('.pkpButton:contains("View")').click();
+        });
+        
+        cy.get('#ui-id-3').click();
+        cy.get('a[id^=component-grid-users-reviewer-reviewergrid-addReviewer-button-]').click();
+        cy.get('[description=""] > .listPanel > .listPanel__header > .pkpHeader > .pkpHeader__actions > .pkpButton').click();
+        cy.get(':nth-child(7) > .pkpFilter > label > input').click();
+		cy.contains('Adela Gallego');
+    })
+});
