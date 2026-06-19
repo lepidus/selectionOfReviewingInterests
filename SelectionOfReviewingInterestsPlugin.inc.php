77c98ef3709eed61fbce20e38ee4264f87da818f
CarlosVercosaLepidus <carlos@lepidus.com.br>
2026-06-19 18:38:21 -0400

    Fix plugin behavior when reviewing interest options are empty
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#1203
    
    Signed-off-by: carlos <carlos@lepidus.com.br>
    Signed-off-by: yves <yves@lepidus.com.br>

--- SelectionOfReviewingInterestsPlugin.inc.php
+++ SelectionOfReviewingInterestsPlugin.inc.php
@@ -12,14 +12,17 @@ class SelectionOfReviewingInterestsPlugin extends GenericPlugin
         $success = parent::register($category, $path);
         if ($success && $this->getEnabled()) {
             $hookCallbacks = new HookCallbacks($this);
-            HookRegistry::register('TemplateManager::display', [$hookCallbacks, 'addChangesOnTemplateDisplaying']);
-            HookRegistry::register('userdetailsform::display', [$hookCallbacks, 'addInterestsScriptsOnUserDetailsFormDisplay']);
-            HookRegistry::register('advancedsearchreviewerform::display', [$hookCallbacks, 'addReviewerInterestFilterOnFormDisplay']);
-            HookRegistry::register('Request::redirect', [$hookCallbacks, 'redirectUserAfterLogin']);
             HookRegistry::register('LoadComponentHandler', [$hookCallbacks, 'setupOptionsConfigurationGridHandler']);
-            HookRegistry::register('API::users::reviewers::params', [$hookCallbacks, 'addReviewerInterestFilterParam']);
-            HookRegistry::register('User::getReviewers::queryBuilder', [$hookCallbacks, 'setReviewerInterestFilter']);
-            HookRegistry::register('User::getMany::queryObject', [$hookCallbacks, 'applyReviewerInterestFilter']);
+
+            if ($this->hasConfiguredInterestOptions()) {
+                HookRegistry::register('TemplateManager::display', [$hookCallbacks, 'addChangesOnTemplateDisplaying']);
+                HookRegistry::register('userdetailsform::display', [$hookCallbacks, 'addInterestsScriptsOnUserDetailsFormDisplay']);
+                HookRegistry::register('advancedsearchreviewerform::display', [$hookCallbacks, 'addReviewerInterestFilterOnFormDisplay']);
+                HookRegistry::register('Request::redirect', [$hookCallbacks, 'redirectUserAfterLogin']);
+                HookRegistry::register('API::users::reviewers::params', [$hookCallbacks, 'addReviewerInterestFilterParam']);
+                HookRegistry::register('User::getReviewers::queryBuilder', [$hookCallbacks, 'setReviewerInterestFilter']);
+                HookRegistry::register('User::getMany::queryObject', [$hookCallbacks, 'applyReviewerInterestFilter']);
+            }
         }
         return $success;
     }
@@ -68,5 +71,24 @@ class SelectionOfReviewingInterestsPlugin extends GenericPlugin
         parent::setEnabled($enabled);
         $templateMgr = TemplateManager::getManager(Application::get()->getRequest());
         $templateMgr->clearCompiledTemplate();
+    }
+
+    private function hasConfiguredInterestOptions()
+    {
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+        if (!$context) {
+            return false;
+        }
+
+        $options = $this->getSetting($context->getId(), 'interestOptions') ?: array();
+        if (!is_array($options)) {
+            return false;
+        }
+
+        $options = array_map('trim', array_values($options));
+        return count(array_filter($options, function ($option) {
+            return $option !== '';
+        })) > 0;
     }
 }
